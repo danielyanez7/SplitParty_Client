23e959eef1239c2a916eaa6ee3aed9c3c43f5a5b
Daniel Yanez <ingdanielyanez@gmail.com>
2023-03-12 15:45:45 +0100

    changes users

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,4 +1,4 @@
-import { Container, Card, Row, Col, Button } from 'react-bootstrap'
+import { Card, Row, Col, Button } from 'react-bootstrap'
 import './UserDetails.css'
 
 const UserDetails = ({ user }) => {
@@ -8,13 +8,13 @@ const UserDetails = ({ user }) => {
         <Card className='card'>
             <div style={{ position: 'absolute', top: 2, right: 2 }}>
                 <Button variant="link">
+                    <img src="https://cdn.icon-icons.com/icons2/2098/PNG/512/user_check_icon_128710.png" alt="Agregar amigo" style={{ width: 20, height: 20 }} />
+                </Button>
+                <Button variant="link">
                     <img src="https://cdn.icon-icons.com/icons2/2098/PNG/512/edit_icon_128873.png" alt="Editar" style={{ width: 20, height: 20, marginRight: 1 }} />
                 </Button>
                 <Button variant="link">
                     <img src="https://cdn.icon-icons.com/icons2/2098/PNG/512/trash_icon_128726.png" alt="Borrar" style={{ width: 20, height: 20 }} />
-                </Button>
-                <Button variant="link" onClick={handleAddFriendClick}>
-                    <img src="https://cdn.icon-icons.com/icons2/2098/PNG/512/user_check_icon_128710.png" alt="Agregar amigo" style={{ width: 20, height: 20 }} />
                 </Button>
             </div>
             <Row className='g-0'>
